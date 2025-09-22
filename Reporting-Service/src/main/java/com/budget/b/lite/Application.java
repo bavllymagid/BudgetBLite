f80a6e4564b6c4dce4bcd6f32fd996934bf91c0c
magidwad <bavlly.magid_wadea.ext@nokia.com>
2025-09-22 12:02:03 +0300

    [ADDED] Service discovery

--- Reporting-Service/src/main/java/com/budget/b/lite/Application.java
+++ Reporting-Service/src/main/java/com/budget/b/lite/Application.java
@@ -1,4 +1,4 @@
-package java.com.budget.b.lite;
+package com.budget.b.lite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
